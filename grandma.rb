6c7852ff18fcb5beef2bd86c1977e168150189b3
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 18:27:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
-
-def speak_to_grandma(phrase)
-  phrase = 'I LOVE YOU GRANDMA!'
-  if phrase == phrase.upcase
-    puts 'I LOVE YOU TOO PUMPKIN'
-  elsif phrase == 'I LOVE YOU GRANDMA!'
-  puts 'NO, NOT SINCE 1938!'
-  else 'HUH?! SPEAK UP, SONNY!'
-end
+if phrase == phrase.upcase 
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end	  
+  end
