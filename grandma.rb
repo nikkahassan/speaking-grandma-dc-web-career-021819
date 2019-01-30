4f830d17f3fccf558aea97f5b6233cd2d7dd795e
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 18:31:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
-def speak_to_grandma(phrase=  "I LOVE YOU GRANDMA!")
-if phrase == phrase.upcase 
-    puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.upcase
+input_phrase = "Hi Nana, how are you?"
+  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do	def speak_to_grandma(phrase)  
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'	  # Check if string equals same phrase all upper case letters, which means string is all uppercase
+  if phrase == phrase.upcase 
+    # return this string if condition is true 
     puts "NO, NOT SINCE 1938!"
-  else
+  else 
+    # return this string if condition is false 
     puts "HUH?! SPEAK UP, SONNY!"
-  end	  
-  end
+  end	  end
