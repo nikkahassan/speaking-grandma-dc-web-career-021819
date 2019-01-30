d65909aee03c8e8a884e9df3a7170888d0987b7d
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 18:33:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
-input_phrase = "Hi Nana, how are you?"
-   if phrase == phrase.upcase 
-   puts "NO, NOT SINCE 1938!"
-  else 
-    puts "HUH?! SPEAK UP, SONNY!"
-  end	  
-  end
+def speak_to_grandma(phrase)
+  if phrase === phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+   end
+end
