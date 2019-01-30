7a06c6a77d00259ad7bb52bf10b2d8574096a280
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 18:39:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase === phrase.upcase
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.upcase 
+    elsif  
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
