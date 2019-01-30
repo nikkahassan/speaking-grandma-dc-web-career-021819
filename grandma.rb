1aef85f75bf1e87b236611349316c776ea90d71c
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 18:41:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase === phrase.upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if(phrase=="I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif (phrase=="#{phrase}".upcase)
+    return "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
-   end
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
